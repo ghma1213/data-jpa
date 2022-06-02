6aa376af28d9ab87dd1fec022976f33688157025
Gyeonghwan <ghma1213@gmail.com>
2022-06-03 08:49:51 +0900

    사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -269,4 +269,9 @@ class MemberRepositoryTest {
         List<Member> findMember = memberRepository.findLockByUsername("member1");
 
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
